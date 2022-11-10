371c30e4eed78f90cf6d04ae9863ad5771d4ff43
GastonInn <gaston.i.0201@gmail.com>
2022-11-10 17:03:40 -0300

    commit del parcial

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Posteos from "./components/Posteos"
+import Estatus from "./components/Estatus"
+// El componente App es el padre de:
+// - Estatus
+// - Posteos
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de likes.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Post.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos para que manipulen o lean su estado.
+
+function App() {
+
+    const [countTotalLikes, setCountTotalLikes] = useState(0);
+
+    const increaseLikes = () => {
+        setCountTotalLikes(countTotalLikes + 1);
+    }
+
+    return (
+    <div className="App">
+        <body>
+            <nav>
+                <Estatus countTotalLikes={countTotalLikes}/>
+            </nav>
+            <Posteos increaseLikes={increaseLikes}/>
+        </body>
+    </div>
+    );
+}
+
+export default App;
